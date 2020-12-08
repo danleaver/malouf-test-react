86306ffa17aaa92dc8bf72f560981e77eb6f8383
Dan Leaver <dleaver@gmail.com>
2020-12-07 18:30:18 -0700

    slider multipic

--- src/components/product-categories/ProductCategories.js
+++ src/components/product-categories/ProductCategories.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css, jsx } from '@emotion/react';
-import { Details, Indicator, Slider, SliderMobile, SliderActionMain, SliderAction,  SlidesContainer, Title, Icon, ChevLeft, ChevRight, DetailsLower, ProductTitle, LowerActionButton, VertLine } from "./productCategoryStyles"
+import { Details, Indicator, Slider, SliderMobile, SliderActionMain, SliderAction,  SlidesContainer, Title, Icon, ChevLeft, ChevRight, DetailsLower, ProductTitle, LowerActionButton, VertLine, Wrapper, UpperWrapper } from "./productCategoryStyles"
 import { useState, useEffect, useRef } from 'react';
 import bedding from '../../assets/product-categories/bedding.png'
 import bases from '../../assets/product-categories/bases.png'
@@ -31,7 +31,8 @@ const ProductCategories = () => {
   const [ animateOver, setAnimateOver ] = useState(false)
 
   const [offset, setOffset] = useState(0)
-
+  const [ slidesRemain, setSlidesRemain ] = useState(0);
+  
   useEffect(() => {
     setTimeout(() => {
       setOffset(sliderRef.current.offsetWidth)
@@ -71,16 +72,30 @@ const ProductCategories = () => {
     setSlidePos(tmpPos)
     setIndicatorPos(tmpPos)
     setAnimateOver(false)
-  
+
+    
+
     //// resets css
       let tmp = sliderRef.current.offsetWidth;
-
+      
+      
    }, [slidePos])
 
   useEffect(() => {
     setIndicatorTitle(pics[ wrapPos(indicatorPos)- 1 ].title)
   }, [indicatorPos])
 
+
+
+  useEffect(() => {
+    if (animateOver === false && slidesRemain !== 0) {
+        slidesRemain > 0 ? setSlidesRemain(slidesRemain - 1) : setSlidesRemain(slidesRemain + 1)
+        slidesRemain > 0 ? nextSlide(1) : nextSlide(-1)
+      } 
+  }, [animateOver])
+
+    
+ 
   const wrapPos = (pos) => {
     if (pos > pics.length) {
       return 1
@@ -91,140 +106,160 @@ const ProductCategories = () => {
   }
 
   const nextSlide = (n, mobile) => {
+    
     if (n === -1) {
       setAnimateInLeft(true)
     } else { 
       setAnimateInRight(true)
     }
 
-    setIndicatorPos(slidePos + n)
+    setIndicatorPos(wrapPos(slidePos + n))
 
     setTimeout(() => {
       setAnimateOver(true)
       setAnimateInRight(false)
       setAnimateInLeft(false)
       setSlidePos(slidePos + n)
-    }, mobile ? 0 : 400)
+      
+    }, mobile ? 0 : 400)    
   }
 
-  const chooseSlide = (k) => {
-    if (k === slidePos - 1) {
+  const chooseSlide = (k, mobile) => {
+    if (k === 1 && slidePos === pics.length) {
+      nextSlide(1)
+    } else if (k === pics.length && slidePos === 1 ) {
+      nextSlide(-1)
+    } else if (k === slidePos - 1) {
       nextSlide(-1)
     } else if (k === slidePos + 1) {
       nextSlide(1)
     } else {
-      setIndicatorPos(k)
-      setSlidePos(k)
+      mobile ? setSlidePos(k) : multipleSlides(k)     
     }
   }
 
+  const multipleSlides = (k) => {
+    if (k - slidePos === 2) {
+      setSlidesRemain(1)
+      nextSlide(1)
+    } else if (k - slidePos === 3) {
+      setSlidesRemain(2)
+      nextSlide(1)
+    } else if (k - slidePos === -2) {
+      setSlidesRemain(-1)
+      nextSlide(-1)
+    } else if (k - slidePos === -3) {
+      setSlidesRemain(-2)
+      nextSlide(-1)
+    } else if (slidePos === 2) {
+      setIndicatorPos(6)
+      setSlidesRemain(-1)
+      nextSlide(-1)
+    } else if (slidePos === 1) {
+      setSlidesRemain(-1)
+      nextSlide(-1)
+    } else if (slidePos === 5) {
+      setSlidesRemain(1)
+      nextSlide(1)
+    } else { //slidePos === 6
+      setSlidesRemain(1)
+      nextSlide(1)
+    }
+      // else {
+      // setIndicatorPos(1)
+      // setSlidesRemain(1)
+      // nextSlide(1)
+    // }
+  }
+
   return (
-    <div
-      css={css`
-        overflow-x: hidden;
-
-        margin-top: -10vw;
-        @media (max-width: 767px) {
-          margin-top: -2rem;
-        }
-      `}
-    >
-      <div
-        css={css`
-          position: relative;
-
-          @media (max-width: 767px) {
-             margin-bottom: -4rem;
-          }
-        `}
-      >
-        <SliderMobile> 
-          {currentSlides.map((slide, idx) => (
-            <>              
-              <SlidesContainer>
-                <img height="100%" src={slide.image} />
-                <div css={css`height: 100%; width: ${idx === 0 ? "20vw" : "10vw"} `}/>
-              </SlidesContainer>
-            </>
-          ))} 
-        </SliderMobile>
-        
-        <Slider 
-          offset={offset} animateOver={animateOver} animateInLeft={animateInLeft} animateInRight={animateInRight}        
-        >
-          {currentSlides.map ((slide, idx) => (
-            <>              
-            <SlidesContainer ref={sliderRef}>
+    <Wrapper>
+      <UpperWrapper>
+      <SliderMobile> 
+        {currentSlides.map((slide, idx) => (
+          <>              
+            <SlidesContainer>
               <img height="100%" src={slide.image} />
-              <Details>
-                <div 
-                  css={css`
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: flex-end;
-                    width: 100%;
-                  `
-                }>
-                <div 
-                  css={css`
-                    font-family: "Mercury-Text-G2";
-                    font-style: italic;
-                    font-size: 16px;                   
-                  `}
-                >
-                  Product Categories
-                </div>                  
-                <div 
-                  onClick={()=>nextSlide(1)}
-                  css={css`
-                    margin: 2rem 0;
+              <div css={css`height: 100%; width: ${idx === 0 ? "20vw" : "10vw"} `}/>
+            </SlidesContainer>
+          </>
+        ))} 
+      </SliderMobile>
+      <Slider 
+        offset={offset} animateOver={animateOver} animateInLeft={animateInLeft} animateInRight={animateInRight}        
+      >
+        {currentSlides.map ((slide, idx) => (
+          <>              
+          <SlidesContainer ref={sliderRef}>
+            <img height="100%" src={slide.image} />
+            <Details>
+              <div 
+                css={css`
+                  display: flex;
+                  justify-content: space-between;
+                  align-items: flex-end;
+                  width: 100%;
+                `
+              }>
+              <div 
+                css={css`
+                  font-family: "Mercury-Text-G2";
+                  font-style: italic;
+                  font-size: 16px;                   
+                `}
+              >
+                Product Categories
+              </div>                  
+              <div 
+                onClick={()=>nextSlide(1)}
+                css={css`
+                  margin: 2rem 0;
 
-                    @media (max-width: 1400px) {
-                      display: none;
-                    }
-                  `}
-                >
-                  <img height="144px" alt="next category" src={nextCat}></img>
-                </div>
-                </div>
-                <div
-                  css={css`
-                    font-size: 26px;
-                    font-family: "Geograph-Bold";
-                    line-height: 28px;
-                    margin: -1.5rem 0 0;
+                  @media (max-width: 1400px) {
+                    display: none;
+                  }
+                `}
+              >
+                <img height="144px" alt="next category" src={nextCat}></img>
+              </div>
+              </div>
+              <div
+                css={css`
+                  font-size: 26px;
+                  font-family: "Geograph-Bold";
+                  line-height: 28px;
+                  margin: -1.5rem 0 0;
 
-                    @media (max-width: 1023px) {
-                      font-size: 20px;
-                    }  
-                  `}
-                >       
-                  {slide.title}
-                </div>
-                <div
-                  css={css`
-                    font-family: Mercury-Text-G2;
+                  @media (max-width: 1023px) {
+                    font-size: 20px;
+                  }  
+                `}
+              >       
+                {slide.title}
+              </div>
+              <div
+                css={css`
+                  font-family: Mercury-Text-G2;
 
-                    @media (max-width: 1023px) {
-                      max-width: 350px;
-                    }
+                  @media (max-width: 1023px) {
+                    max-width: 350px;
+                  }
 
-                    @media (max-width: 868px) {
-                      max-width: 280px;
-                    }
-                  `}
-                >
-                  Here is copy that provides a description of this feel and material. Also brief mention of how our material is different. Here is copy that provides a description of this feel and material. Also brief mention of how our material is different. 
-                </div>
-                <LowerActionButton>
-                  Learn More &nbsp; ⟶
-                </LowerActionButton>
-              </Details>
-             </SlidesContainer>
-            </>
-          ))}
-        </Slider>
-      </div>
+                  @media (max-width: 868px) {
+                    max-width: 280px;
+                  }
+                `}
+              >
+                Here is copy that provides a description of this feel and material. Also brief mention of how our material is different. Here is copy that provides a description of this feel and material. Also brief mention of how our material is different. 
+              </div>
+              <LowerActionButton>
+                Learn More &nbsp; ⟶
+              </LowerActionButton>
+            </Details>
+            </SlidesContainer>
+          </>
+        ))}
+      </Slider>
       {/* for tablet/desktop */}      
       <div
         css={css`
@@ -255,7 +290,7 @@ const ProductCategories = () => {
       <Title>
           Product Categories
       </Title>   
-      
+      </UpperWrapper>
       {/* MOBILE */}
       <div>      
         {/* for mobile slider */}
@@ -283,14 +318,14 @@ const ProductCategories = () => {
           >
             { [...Array(6)].map( (tmp, idx) => (
               <Indicator
-                onClick={() => chooseSlide(idx + 1)}
+                onClick={() => chooseSlide(idx + 1, true)}
                 indicatorPos={indicatorPos} pos={idx + 1}
               /> 
             ))}
           </div>
         </DetailsLower>
       </div>
-    </div>
+    </Wrapper>
   );
 }
 
--- src/components/product-categories/productCategoryStyles.js
+++ src/components/product-categories/productCategoryStyles.js
@@ -3,40 +3,8 @@ import furniture from '../../assets/product-categories/icons/furniture.svg'
 import chevLeft from '../../assets/product-categories/chev-left.svg'
 import chevRight from '../../assets/product-categories/chev-right.svg'
 
-export const Title = styled.div`
-  transform-origin: top left;
-  transform: rotate(-90deg);
-  width: 12rem;
-  height: 4.5rem;
-  flex-shrink: 0;
-  margin-bottom: 0;
-  margin-left: 0rem;
-  font-style: italic;
-  position: relative;
-  top: -6rem;
-  right: -2rem;
-  font-family: Mercury-Text-G2;
-  display: flex;
-  justify-content: space-between;
-  text-align: right;
-  font-size: 20px;
 
-  @media (min-width: 768px) { 
-    display: none;
-    margin-right: 0;
-  }
-`
 
-export const Icon = styled.div`
-  background-image: url(${furniture});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 50px;
-  width: 53px;
-  filter: opacity(35%);
-  cursor: zoom-in;
-`
 export const ChevLeft = styled.div`
   background: url(${chevLeft}) center no-repeat;
   background-size: cover;
@@ -54,110 +22,6 @@ export const ChevRight = styled.div`
 
   &:hover {
     transform: scale3d(10);
-  }
-`
-
-export const SliderAction = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-
-  @media (min-width: 768px) {
-    display: none;
-  }
-`
-
-export const SliderActionMain = styled.div`
-
-  display: flex;
-  width: 40rem;
-  justify-content: space-between;
-  align-items: center;
-
-  @media (max-width: 767px) {
-    display: none;
-  }
-`
-
-export const DetailsLower = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  @media (min-width: 768px) {
-    display: none;
-  }
-`
-
-export const ProductTitle = styled.div`
-  font-family: "Geograph-500";
-  font-size: 20px;
-`
-
-export const LowerActionButton = styled.div`
-  background: #1e1e1e;
-  width: 180px;
-  height: 45px;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: "Mercury-Text-G2";
-  font-style: italic; 
-`
-
-export const VertLine = styled.div`
-  width: 1px;
-  height: 85px;
-  background: black;
-`
-
-export const Indicator = styled.div`
-  background: grey;
-  border-radius: 50%;
-  height: 14px;
-  width: 14px;
-  cursor: pointer;
-  margin: 0 0.25rem;
-  ${props=> props.pos === props.indicatorPos && "background: #1E1e1e;"}
-`
-
-export const Slider = styled.div`
-  display: flex;
-  align-items: flex-end;
-  position: relative;
-  right: 85%;
-  height: 51vw; 
-  max-height: 735px;
-  
-  @media (max-width: 767px) {
-    display: none;
-  }
-  
-  transition: transform .35s ease-in-out;
-  ${props => props.animateInRight && `transform: translateX(-${props.offset}px);`}
-  ${props => props.animateInLeft && `transform: translateX(+${props.offset}px);`}
-  ${props => props.animateOver && "transition: none; transform: translateX(0);"}
-
-  }
-`
-
-export const SlidesContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-`
-
-export const SliderMobile = styled.div`
-  display: flex;
-  height: 80vw;
-  position: relative;
-  right: calc(50% + 10vw);
-
-  @media (min-width: 768px) {
-    display: none;
   }
 `
 
@@ -227,5 +91,155 @@ export const Details = styled.div`
 
   @media (max-width: 767px) {
     display: none;
+  }
+`
+
+export const DetailsLower = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const Icon = styled.div`
+  background-image: url(${furniture});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 50px;
+  width: 53px;
+  filter: opacity(35%);
+  cursor: zoom-in;
+`
+
+export const Indicator = styled.div`
+  background: grey;
+  border-radius: 50%;
+  height: 14px;
+  width: 14px;
+  cursor: pointer;
+  margin: 0 0.25rem;
+  ${props=> props.pos === props.indicatorPos && "background: #1E1e1e;"}
+`
+
+export const LowerActionButton = styled.div`
+  background: #1e1e1e;
+  width: 180px;
+  height: 45px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Mercury-Text-G2";
+  font-style: italic; 
+`
+
+export const ProductTitle = styled.div`
+  font-family: "Geograph-500";
+  font-size: 20px;
+`
+
+export const SliderAction = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const SliderActionMain = styled.div`
+  display: flex;
+  width: 40rem;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`
+
+export const Slider = styled.div`
+  display: flex;
+  align-items: flex-end;
+  position: relative;
+  right: 85%;
+  height: 51vw; 
+  max-height: 735px;
+  transition: transform .35s ease-in-out;
+  ${props => props.animateInRight && `transform: translateX(-${props.offset}px);`}
+  ${props => props.animateInLeft && `transform: translateX(+${props.offset}px);`}
+  ${props => props.animateOver && "transition: none; transform: translateX(0);"}
+  
+  @media (max-width: 767px) {
+    display: none;
+  }
+`
+
+export const SlidesContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+`
+
+export const SliderMobile = styled.div`
+  display: flex;
+  height: 80vw;
+  position: relative;
+  right: calc(50% + 10vw);
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+export const Title = styled.div`
+  transform-origin: top left;
+  transform: rotate(-90deg);
+  width: 12rem;
+  height: 4.5rem;
+  flex-shrink: 0;
+  margin-bottom: 0;
+  margin-left: 0rem;
+  font-style: italic;
+  position: relative;
+  top: -6rem;
+  right: -2rem;
+  font-family: Mercury-Text-G2;
+  display: flex;
+  justify-content: space-between;
+  text-align: right;
+  font-size: 20px;
+
+  @media (min-width: 768px) { 
+    display: none;
+    margin-right: 0;
+  }
+`
+
+export const VertLine = styled.div`
+  width: 1px;
+  height: 85px;
+  background: black;
+`
+
+export const Wrapper = styled.div`
+  overflow-x: hidden;
+
+  margin-top: -10vw;
+  @media (max-width: 767px) {
+    margin-top: -2rem;
+  }
+`
+
+export const UpperWrapper = styled.div`
+  @media (max-width: 767px) {
+    margin-bottom: -4rem;
   }
 `
